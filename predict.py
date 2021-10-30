9c6b80c699ae7f8cee3aa737e631d4bd3607fd7f
Chenxi <chenxi@Chenxis-MacBook-Pro-2.local>
2021-10-30 23:11:03 +0100

    add option to output halftone only

--- predict.py
+++ predict.py
@@ -20,18 +20,27 @@ class Predictor(cog.Predictor):
         help="input image. Output will be the dithered input image (halftone) and the restored image "
         "from the generated halftone.",
     )
-    def predict(self, image):
+    @cog.input(
+        "halftone_only",
+        type=bool,
+        default=True,
+        help="choose to only output the dithered input image (halftone), or both halftone and the restored image",
+    )
+    def predict(self, image, halftone_only):
         out_path = Path(tempfile.mkdtemp()) / "out.png"
         color = mmcv.imread(str(image), flag="color") / 127.5 - 1.0
         refHalf = mmcv.imread(str(image), flag="grayscale") / 127.5 - 1.0
         h, c = self.invhalfer(util.img2tensor(color), util.img2tensor(refHalf))
         h = util.tensor2img(h / 2.0 + 0.5) * 255.0
         c = util.tensor2img(c / 2.0 + 0.5) * 255.0
-        mmcv.imwrite(h, "halftone.png")
-        mmcv.imwrite(c, "restored.png")
-        # concatenate gray and color image using opencv
-        halftone = cv2.imread("halftone.png")
-        restored = cv2.imread("restored.png")
-        result = cv2.vconcat([restored, halftone])
-        cv2.imwrite(str(out_path), result)
+        if halftone_only:
+            mmcv.imwrite(h, str(out_path))
+        else:
+            mmcv.imwrite(h, "halftone.png")
+            mmcv.imwrite(c, "restored.png")
+            # concatenate gray and color image using opencv
+            halftone = cv2.imread("halftone.png")
+            restored = cv2.imread("restored.png")
+            result = cv2.vconcat([halftone, restored])
+            cv2.imwrite(str(out_path), result)
         return out_path
